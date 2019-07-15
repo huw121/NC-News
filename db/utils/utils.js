aa0c1744ec64394e2fe620c89cd72a95f3aa9379
Huw Jones <huwjones025@gmail.com>
2019-07-15 13:07:44 +0100

    finished writing and testing formatDates function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,13 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  return list.map(object => {
+    const { timestamp, ...restOfObject } = object;
+    return timestamp ? {
+      timestamp: new Date(timestamp),
+      ...restOfObject
+    } : { ...restOfObject }
+  });
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => { };
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => { };
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -5,7 +5,43 @@ const {
   formatComments,
 } = require('../db/utils/utils');
 
-describe('formatDates', () => {});
+describe('formatDates', () => {
+  it('takes an empty array and returns a new empty array', () => {
+    const input = [];
+    const actual = formatDates(input);
+    const expected = [];
+    expect(actual).to.eql(expected);
+    expect(actual).to.not.equal(input);
+  });
+  describe('it takes an array containing one object and...', () => {
+    it('returns an array containing a new object', () => {
+      const obj = {};
+      const input = [obj];
+      const actual = formatDates(input);
+      const expected = [{}];
+      expect(actual).to.eql(expected);
+      expect(expected[0]).to.not.equal(obj);
+    });
+    it('returns an array containing an object with all the same properties as the object passed', () => {
+      const obj = {title: "hello", body: "some interesting stuff"};
+      const input = [obj];
+      const actual = formatDates(input);
+      expect(actual[0]).to.have.all.keys(Object.keys(obj));
+    });
+    it('must convert the timestamp property to a javascript date object', () => {
+      const obj = {timestamp: 1542284514171};
+      const input = [obj];
+      const actual = formatDates(input);
+      expect(actual[0].timestamp instanceof Date ).to.be.true;
+    });
+  });
+  it('must do for an array of many objects, what it does for one', () => {
+    const input = [{title: "hi", timestamp: 1542284514171}, {body: 'hello my name is', timestamp: 1542284514999}];
+    const actual = formatDates(input);
+    const expected = [{title: "hi", timestamp: new Date(1542284514171)}, {body: 'hello my name is', timestamp: new Date(1542284514999)}];
+    expect(actual).to.eql(expected);
+  });
+});
 
 describe('makeRefObj', () => {});
 
